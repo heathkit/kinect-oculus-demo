7ebc44f9056e18ed521fbb0671cf83d5ff8b7257
Michael Giambalvo <heathkit@gmail.com>
2013-10-29 15:51:59 -0700

    Initial commit

---
+++ Assets/BallBoundary.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallBoundary : MonoBehaviour {
+	
+	public GameObject ballSpawnLocation;
+	public GameObject ballPrefab;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter( Collider other)
+	{
+		if( other.tag == "ball")
+		{
+			Destroy(other.gameObject); // destroy this ball
+			Instantiate( ballPrefab, ballSpawnLocation.transform.position, ballSpawnLocation.transform.rotation);
+		}
+	}
+}
